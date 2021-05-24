7461cfac85dc3d876e8708c316b7e84e72ddb213
Alex Kim <alexkim251@yahoo.com>
2021-05-24 13:19:17 -0500

    wrote base for getjokes function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,21 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+    getJokes();
+}
+
+//Get jokes from server and display on DOM
+function getJokes() {
+    $.ajax({
+        method: 'get',
+        url:'/jokes'
+    }).then(function (response)){
+        console.log(response)
+        
+        //empty DOM
+        $('#outputDiv').empty();
+
+
+    }
 }
